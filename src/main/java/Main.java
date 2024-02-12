14b87eb18136338d015ad756a7f8a767e95d2919
nswangarcia <nswanso6@asu.edu>
2024-02-12 02:27:46 -0600

    Add src and subfolders

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+/**
+ * File: Main.java.
+ * Author: Nicole Garcia
+ * Date: 02/12/2024
+ */
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    /**
+     * Main constructor.
+     */
+    public Main() {
+    }
+
+    /**
+     * Main method.
+     * @param args main arguments
+     */
+    public static void main(String[] args) {
+        init();
+    }
+
+    /**
+     * Initialize The Mines of Forsyth.
+     * @return boolean
+     */
+    public static boolean init() {
+        System.out.println("***************************************");
+        System.out.println("*             WELCOME TO              *");
+        System.out.println("*        THE MINES OF FORSYTH:        *");
+        System.out.println("*        DUNGEON CRAWLER GAME         *");
+        System.out.println("***************************************");
+        return true;
+    }
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,24 @@
+/**
+ * File: TestMain.java.
+ * Author: Nicole Garcia
+ * Date: 02/12/2024
+ */
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * TestMain class.
+ */
+public class TestMain {
+
+    /**
+     * Test main init method.
+     */
+    @Test
+    public void testInit() {
+        Main main = new Main();
+        assertTrue(main.init());
+    }
+}
