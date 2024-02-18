04aa733f1ad6caa77334a1d70856e91646b66247
nswangarcia <nswanso6@asu.edu>
2024-02-18 01:14:43 -0600

    Add final boss

---
+++ src/main/java/enemies/DragonKingFinalBoss.java
@@ -0,0 +1,68 @@
+/**
+ * File: DragonKing.java.
+ * Author: Nicole Garcia
+ * Date: 02/17/2024
+ */
+package enemies;
+
+/**
+ * DragonKing Final Boss class
+ */
+public class DragonKingFinalBoss extends Enemy {
+
+    private int enemyHealth;
+    private int enemyMaxHealth;
+    private int enemyMaxDamageDealt;
+    private int enemyMinHealth;
+    private int enemyMinDamageDealt;
+
+    private int enemySpeed;
+
+    public DragonKingFinalBoss(int enemyHealth, int enemyMaxHealth, int enemyMaxDamageDealt, int enemyMinHealth,
+                               int enemyMinDamageDealt, int enemySpeed) {
+        this.enemyHealth = enemyHealth;
+        this.enemyMaxHealth = enemyMaxHealth;
+        this.enemyMaxDamageDealt = enemyMaxDamageDealt;
+        this.enemyMinHealth=enemyMinHealth;
+        this.enemyMinDamageDealt = enemyMinDamageDealt;
+        this.enemySpeed = enemySpeed;
+    }
+
+    @Override
+    public void setEnemyHealth(int healthPoints) {
+        this.enemyHealth = healthPoints;
+    }
+
+    @Override
+    public int getEnemyHealth() {
+        return this.enemyHealth;
+    }
+
+    @Override
+    public int getEnemyMaxHealth() {
+        return this.enemyMaxHealth;
+    }
+
+    @Override
+    public int getEnemyMaxDamageDealt() {
+        return this.enemyMaxDamageDealt;
+    }
+
+    @Override
+    public int getEnemyMinHealth() {
+        return this.enemyMinHealth;
+    }
+
+    @Override
+    public int getEnemyMinDamageDealt() {
+        return this.enemyMinDamageDealt;
+    }
+
+    @Override
+    public int getEnemySpeed() {
+        return this.enemySpeed;
+    }
+}
+
+
+
